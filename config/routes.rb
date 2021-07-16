1ec1621db40ca15c3fb29456b91bae6ce8dc5ccb
Jovanni Luna <jluna@foursquare.com>
2021-07-16 13:35:53 -0400

    changed routes to users#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :users
-  root 'application#hello'
+  # root 'application#hello'
+  # above routes the default url to our application and into the action of hello which prints the html of hello world
+  root 'users#index'
+  #above routes to the users router instead in our default url, # represent which route in that router which is our index that shows all users
 end
