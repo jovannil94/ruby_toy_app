7580164ebb401c5fa5a07daf7cfb527dac602c26
Jovanni Luna <jluna@foursquare.com>
2021-07-16 13:49:50 -0400

    created micropost db, migrated into ruby app

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  resources :microposts
   resources :users
   # root 'application#hello'
   # above routes the default url to our application and into the action of hello which prints the html of hello world
   root 'users#index'
-  #above routes to the users router instead in our default url, # represent which route in that router which is our index that shows all users
+  #above routes to the users router instead in our default url, # represent which action in that router which is our index that shows all users
 end
